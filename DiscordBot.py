e7c205b980df2444d2328afa958a174d30481c7a
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2020-06-30 20:12:51 +1000

    Added functionality for background task to push updates in the future

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,18 +1,40 @@
 import discord
 from discord.ext import commands
+from discord.ext.tasks import loop
 import json
 import os
+import shutil
 
 
 bot = commands.Bot(command_prefix='!', description='F1 data streaming bot.')
-bot_testing_channel = 1234567890
 
 with open(os.path.join(os.getcwd(), 'credentials.json')) as f:
-  credentials = json.loads(f.read())["discord"]
+  credentials = json.loads(f.read())['discord']
 
 
-async def raceStart():
-    channel = bot.get_channel(bot_testing_channel)
-    await channel.send("pong")
+@loop(seconds=5)
+async def post_update():
+  '''Posts an update to discord composing of data, images, etc'''
+  temp_data = os.path.join(os.getcwd(), 'temp_data')
+  if os.path.exists(temp_data):
 
-bot.run(credentials["token"])
+    print('Pushing update...')
+    '''
+    Implement functionality to parse and post data within a directory called temp_data
+    temp_data may contain:
+      csv of race data
+      graphs and graphics
+      etc
+    '''
+
+    channel = bot.get_channel(credentials['f1-channel'])
+    shutil.rmtree(temp_data)
+
+
+@bot.event
+async def on_ready():
+    '''Spools up services/background tasks for discord bot'''
+    post_update.start()
+
+
+bot.run(credentials['token'])
