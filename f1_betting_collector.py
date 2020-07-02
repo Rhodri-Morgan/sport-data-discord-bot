f4c8ccce1f7346db991b56660412f7eab0fd239c
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2020-07-02 15:44:54 +1000

    Overhauled to betting collector now acting as an interactable class

--- f1_betting_collector.py
+++ f1_betting_collector.py
@@ -5,38 +5,36 @@ import datetime
 import pandas as pd
 
 
-betfair = betfair_api.BetFairAPI()
-motorsport_events = betfair.get_event_id('Motor Sport')
-f1_meta = f1_meta_collector.F1MetaCollector()
-f1_markets = f1_betfair_markets.F1BetfairMarkets(betfair, motorsport_events)
-price_data = 'EX_BEST_OFFERS'            
+class F1BettingCollector:
+    def __init__(self): 
+        self.betfair = betfair_api.BetFairAPI()
+        self.motorsport_events = self.betfair.get_event_id('Motor Sport')
+        self.f1_meta = f1_meta_collector.F1MetaCollector()
+        self.f1_markets = f1_betfair_markets.F1BetfairMarkets(self.betfair, self.motorsport_events)
+        self.price_data = 'EX_BEST_OFFERS'            
 
 
-def get_championship_outright_winner_probabilities():
-    ''' Gets the outright championship probabilities for all markets, returns dict with market name as key and proabilities as values '''
-    outright_dict = {}
-    outright_markets = f1_markets.get_f1_outright_championship_markets()
-    for row in range(outright_markets.shape[0]):
-        outright_market_id = outright_markets.iloc[row]['Market ID']
-        outright_market_name = outright_markets.iloc[row]['Market Name']
-        outright_runners = betfair.get_runners_market_data(outright_market_id, price_data)
-        outright_probabilities = betfair.covert_price_to_probability(outright_runners)
-        outright_dict[outright_market_name] = outright_probabilities
-    return outright_dict
+    def get_championship_outright_winner_probabilities(self):
+        ''' Gets the outright championship probabilities for all markets, returns dict with market name as key and proabilities as values '''
+        outright_dict = {}
+        outright_markets = self.f1_markets.get_f1_outright_championship_markets()
+        for row in range(outright_markets.shape[0]):
+            outright_market_id = outright_markets.iloc[row]['Market ID']
+            outright_market_name = outright_markets.iloc[row]['Market Name']
+            outright_runners = self.betfair.get_runners_market_data(outright_market_id, self.price_data)
+            outright_probabilities = self.betfair.covert_price_to_probability(outright_runners)
+            outright_dict[outright_market_name] = outright_probabilities
+        return outright_dict
 
 
-def get_next_race_probabilities():
-    next_race_dict = {}
-    next_race_datetime = f1_meta.get_race_datetime(f1_meta.get_next_race())
-    next_race_markets = f1_markets.get_next_f1_race_markets(next_race_datetime)
-    for row in range(next_race_markets.shape[0]):
-        next_race_market_id = next_race_markets.iloc[row]['Market ID']
-        next_race_market_name = next_race_markets.iloc[row]['Market Name']
-        next_race_runners = betfair.get_runners_market_data(next_race_market_id, price_data)
-        next_race_probabilities = betfair.covert_price_to_probability(next_race_runners)
-        next_race_dict[next_race_market_name] = next_race_probabilities
-    return next_race_dict
-
-
-championship_outright_winner_probabilities = get_championship_outright_winner_probabilities()
-next_race_probabilities = get_next_race_probabilities()
+    def get_next_race_probabilities(self):
+        next_race_dict = {}
+        next_race_datetime = self.f1_meta.get_race_datetime(self.f1_meta.get_next_race())
+        next_race_markets = self.f1_markets.get_next_f1_race_markets(next_race_datetime)
+        for row in range(next_race_markets.shape[0]):
+            next_race_market_id = next_race_markets.iloc[row]['Market ID']
+            next_race_market_name = next_race_markets.iloc[row]['Market Name']
+            next_race_runners = self.betfair.get_runners_market_data(next_race_market_id, self.price_data)
+            next_race_probabilities = self.betfair.covert_price_to_probability(next_race_runners)
+            next_race_dict[next_race_market_name] = next_race_probabilities
+        return next_race_dict
