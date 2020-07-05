ef3c8024f8c0478f0f1ceff42e872021a08f4a98
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2020-07-05 22:10:13 +1000

    Added functionality to save data daily

--- discord_bot.py
+++ discord_bot.py
@@ -1,44 +1,66 @@
-import f1_betting_collector
+import json
+import os
+import shutil
+from datetime import datetime, timedelta
+
 import discord
 from discord.ext import commands
 from discord.ext.tasks import loop
-import json
-import regex
-import os
-import shutil
-import time
+
+import f1_betting_collector
+from f1_data_logger import *
 
 
 bot = commands.Bot(command_prefix='!')
+
+f1_betting_collector = f1_betting_collector.F1BettingCollector()
+
+datalogger_datetime = datetime(datetime.utcnow().year, datetime.utcnow().month, datetime.utcnow().day, 00, 00)
 
 with open(os.path.join(os.getcwd(), 'credentials.json')) as f:
     credentials = json.loads(f.read())['discord']
 
 
+@loop(seconds=1)
+async def f1_data_logger():
+    ''' Saves and logs data at 0000 UTC '''
+    if datetime.utcnow().replace(second=0, microsecond=0) == datalogger_datetime:
+        print('RUNNING: f1_data_logger()')
+        print(datetime.utcnow())
+        f1_outright_winners = f1_betting_collector.get_championship_outright_winner_probabilities()
+        print(f1_outright_winners)
+        save_world_constructors_champion_data({datetime.utcnow().strftime('%Y-%m-%d'): f1_outright_winners['Winner - Constructors Championship']})
+        save_world_drivers_champion_data({datetime.utcnow().strftime('%Y-%m-%d'): f1_outright_winners['Winner - Drivers Championship']})    
+        datalogger_datetime = datalogger_datetime + timedelta(days=1)
+        print('COMPLETED: f1_data_logger()')
+
+
 @bot.command()
-async def status(ctx):
+async def f1_status(ctx):
     ''' Prints available markets for the user to view '''
+    print('RUNNING: f1_status()')
     channel = bot.get_channel(credentials['f1-channel'])
     async with ctx.typing():
         motorsport_events =  f1_betting_collector.motorsport_events
-        motorsport_events_description = ""
+        motorsport_events_description = ''
         for event in motorsport_events:
             motorsport_events_description = motorsport_events_description + event.event.name + '\n'
         
         outright_name, outright_str = f1_betting_collector.get_championship_outright_winner_str()
         next_race_name, next_race_str = f1_betting_collector.get_next_race_str()
 
-        e = discord.Embed(title="Motorsport Events", description=motorsport_events_description, color=0xFFFF00)
+        e = discord.Embed(title='Motorsport Events', description=motorsport_events_description, color=0xFFFF00)
         e.add_field(name=outright_name, value=outright_str, inline=False)
         e.add_field(name=next_race_name, value=next_race_str, inline=False)
     await channel.send(embed=e)
+    print('COMPLETED: f1_status()')
 
 
-@bot.command()
-async def test(ctx):
-    channel = bot.get_channel(credentials['f1-channel'])
-    await channel.send("testing multiprocessing!")
+@bot.event
+async def on_ready():
+    '''Spools up services/background tasks for discord bot'''
+    print('Discord bot: ONLINE')
+    f1_data_logger.start()
+    
 
-
-f1_betting_collector = f1_betting_collector.F1BettingCollector()
 bot.run(credentials['token'])
--- f1_data_logger.py
+++ f1_data_logger.py
@@ -1,37 +1,32 @@
-from num2words import num2words
-import datetime
-import os
 import json
-import math
+import os
 
 
-class F1DataProcessor:
-    def __init__(self): 
-        self.world_drivers_champion_json = os.path.join(os.getcwd(), 'historic_data/world_drivers_champion.json')
-        self.world_constructors_champion_json = os.path.join(os.getcwd(), 'historic_data/world_constructors_champion.json')
+world_constructors_champion_json = os.path.join(os.getcwd(), 'historic_data/world_constructors_champion.json')
+world_drivers_champion_json = os.path.join(os.getcwd(), 'historic_data/world_drivers_champion.json')
 
 
-    def save_world_drivers_champion_data(self, world_drivers_champion_probabilities):
-        ''' Saves world drivers champion probability data to JSON file in historic_data directory '''
-        if not os.path.exists(self.world_drivers_champion_json):
-            with open(self.world_drivers_champion_json, 'w') as f:
-                json.dump(world_drivers_champion_probabilities, f)
-        else:
-            with open(self.world_drivers_champion_json) as f:
-                world_drivers_champion_data = json.load(f)
-            world_drivers_champion_merged = {**world_drivers_champion_data, **world_drivers_champion_probabilities}
-            with open(self.world_drivers_champion_json, "w") as f:
-                json.dump(world_drivers_champion_merged, f)
+def save_world_constructors_champion_data(world_constructors_champion_probabilities):
+    ''' Saves world constructors champion probability data to JSON file in historic_data directory '''
+    if not os.path.exists(world_constructors_champion_json):
+        with open(world_constructors_champion_json, 'w') as f:
+            json.dump(world_constructors_champion_probabilities, f)
+    else:
+        with open(world_constructors_champion_json) as f:
+            world_constructors_champion_data = json.load(f)
+        world_constructors_champion_merged = {**world_constructors_champion_data, **world_constructors_champion_probabilities}
+        with open(world_constructors_champion_json, 'w') as f:
+            json.dump(world_constructors_champion_merged, f)
 
 
-    def save_world_constructors_champion_data(self, world_constructors_champion_probabilities):
-        ''' Saves world constructors champion probability data to JSON file in historic_data directory '''
-        if not os.path.exists(self.world_constructors_champion_json):
-            with open(self.world_constructors_champion_json, 'w') as f:
-                json.dump(world_constructors_champion_probabilities, f)
-        else:
-            with open(self.world_constructors_champion_json) as f:
-                world_constructors_champion_data = json.load(f)
-            world_constructors_champion_merged = {**world_constructors_champion_data, **world_constructors_champion_probabilities}
-            with open(self.world_constructors_champion_json, "w") as f:
-                json.dump(world_constructors_champion_merged, f)
+def save_world_drivers_champion_data(world_drivers_champion_probabilities):
+    ''' Saves world drivers champion probability data to JSON file in historic_data directory '''
+    if not os.path.exists(world_drivers_champion_json):
+        with open(world_drivers_champion_json, 'w') as f:
+            json.dump(world_drivers_champion_probabilities, f)
+    else:
+        with open(world_drivers_champion_json) as f:
+            world_drivers_champion_data = json.load(f)
+        world_drivers_champion_merged = {**world_drivers_champion_data, **world_drivers_champion_probabilities}
+        with open(world_drivers_champion_json, 'w') as f:
+            json.dump(world_drivers_champion_merged, f)
