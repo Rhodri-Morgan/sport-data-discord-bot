91cbe6933696f09ea53b8049fd8566147a30b882
Rhys Morgan <misc93.22ks@hotmail.com>
2020-07-16 19:55:19 +0100

    added barchart function

--- graphing/graphing_func.py
+++ graphing/graphing_func.py
@@ -17,14 +17,14 @@ class graphing_func:
 
     def dataset_instances_dates_list(self, json_data):
         dates=[]
-        for key1,value1 in json_data.items():
+        for key1 in json_data.keys():
             dates.append(key1)
         return dates
 
 
     def merged_dictionary(self, json_data):
         merged_dictionary={}
-        for key1,value1 in json_data.items():
+        for value1 in json_data.values():
             for key2,value2 in value1.items():
                 if key2 in merged_dictionary:
                     merged_dictionary[key2].append(np.nan_to_num(value2))
@@ -35,14 +35,14 @@ class graphing_func:
 
     def driver_colors_matcher_loop(self, merged_dictionary):
         colors_lst=[]
-        for key1,value1 in merged_dictionary.items():
+        for key1 in merged_dictionary.keys():
             for key2,value2 in self.driver_colour_directory_data.items():
                 if key1==key2:
                     colors_lst.append(value2)
         return colors_lst
 
 
-    def most_recent_dataset_dictionary(self, json_data):
+    def most_recent_dataset_dictionary_for_pie(self, json_data):
         key1,value1 = max(json_data.items())
         latest_dict=value1
         zerod_dict={}
@@ -50,6 +50,14 @@ class graphing_func:
             zerod_dict[key2] = [np.nan_to_num(value2)]
         return zerod_dict
 
+    def most_recent_dataset_dictionary_for_bar(self, json_data):
+        key1,value1 = max(json_data.items())
+        latest_dict=value1
+        zerod_dict={}
+        for key2,value2 in latest_dict.items():
+            if value2>5:
+                zerod_dict[key2] = np.nan_to_num(value2)
+        return zerod_dict
 
     def convert_dictionary_to_list(self, dictionary):
         lst=[]
@@ -58,22 +66,15 @@ class graphing_func:
         return lst
 
     def stackplot(self, json_data, title, xlabel, ylabel):
-        github_url = 'https://github.com/google/fonts/blob/master/ofl/titilliumweb/TitilliumWeb-Regular.ttf'
-        url = github_url + '?raw=true'
-        response = urlopen(url)
-        f = NamedTemporaryFile(delete=False, suffix='.ttf')
-        f.write(response.read())
-        f.close()
-        prop = fm.FontProperties(fname=f.name, size=15)
         fig, ax=plt.subplots()
-        ax.set_facecolor('#ffffff')
-        fig.set_facecolor('#ffffff')
         plt.stackplot(self.dataset_instances_dates_list(json_data), 
                     self.merged_dictionary(json_data).values(), 
                     labels=self.convert_dictionary_to_list(self.merged_dictionary(json_data)), 
                     colors=self.driver_colors_matcher_loop(self.merged_dictionary(json_data)), 
-                    alpha=1
-                    )
+                    alpha=1)
+        
+        ax.set_facecolor('#ffffff')
+        fig.set_facecolor('#ffffff')
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         ax.spines['bottom'].set_visible(False)
@@ -81,16 +82,8 @@ class graphing_func:
         ax.xaxis.set_ticks_position('none') 
         ax.yaxis.set_ticks_position('none') 
         plt.margins(0, 0, tight=True)
-        plt.yticks([10,20,30,40,50,60,70,80,90,100], fontproperties=prop, size=15)
-        plt.xticks(fontproperties=prop, size=15)
-        plt.title(title, fontproperties=prop, size=30, loc='Center', pad=20)
-        plt.xlabel(xlabel, fontproperties=prop, size=20)
-        plt.ylabel(ylabel, fontproperties=prop, size=20)
-        plt.legend(bbox_to_anchor=(1.15, 0.75), loc='upper right', fancybox=True, frameon=False, prop=prop)
         plt.subplots_adjust(left=0.07, bottom=0.08, right=0.87, top=0.94, wspace=0.2, hspace=0.2)
-        plt.show()
-
-    def piechart(self, json_data, title):
+        
         github_url = 'https://github.com/google/fonts/blob/master/ofl/titilliumweb/TitilliumWeb-Regular.ttf'
         url = github_url + '?raw=true'
         response = urlopen(url)
@@ -98,15 +91,65 @@ class graphing_func:
         f.write(response.read())
         f.close()
         prop = fm.FontProperties(fname=f.name, size=15)
-        sizes = self.most_recent_dataset_dictionary(json_data).values()
-        labels = self.convert_dictionary_to_list(self.most_recent_dataset_dictionary(json_data))
+
+        plt.yticks([10,20,30,40,50,60,70,80,90,100], fontproperties=prop, size=15)
+        plt.xticks(fontproperties=prop, size=15)
+        plt.title(title, fontproperties=prop, size=30, loc='Center', pad=20)
+        plt.xlabel(xlabel, fontproperties=prop, size=20)
+        plt.ylabel(ylabel, fontproperties=prop, size=20)
+        plt.legend(bbox_to_anchor=(1.15, 0.75), loc='upper right', fancybox=True, frameon=False, prop=prop)
+
+        plt.show()
+
+    def piechart(self, json_data, title):
+        sizes = self.most_recent_dataset_dictionary_for_pie(json_data).values()
+        labels = self.convert_dictionary_to_list(self.most_recent_dataset_dictionary_for_pie(json_data))
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes,
                 shadow=False, 
                 colors=self.driver_colors_matcher_loop(self.merged_dictionary(json_data)), 
-                startangle=90
-                )
+                startangle=90)
+
+        github_url = 'https://github.com/google/fonts/blob/master/ofl/titilliumweb/TitilliumWeb-Regular.ttf'
+        url = github_url + '?raw=true'
+        response = urlopen(url)
+        f = NamedTemporaryFile(delete=False, suffix='.ttf')
+        f.write(response.read())
+        f.close()
+        prop = fm.FontProperties(fname=f.name, size=15)
+
         plt.title(title, fontproperties=prop, size=30)
         ax1.legend(labels, loc="center right", fancybox=True, frameon=False, prop=prop)
         ax1.axis('equal')
+
+        plt.show()
+
+    def barchart(self, json_data, title, xlabel, ylabel):
+        fig, ax=plt.subplots()
+        x = self.most_recent_dataset_dictionary_for_bar(json_data).keys()
+        y = self.most_recent_dataset_dictionary_for_bar(json_data).values()
+        plt.bar(x, y, color=self.driver_colors_matcher_loop(self.merged_dictionary(json_data)))
+
+        ax.set_facecolor('#ffffff')
+        fig.set_facecolor('#ffffff')
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.xaxis.set_ticks_position('none') 
+        ax.yaxis.set_ticks_position('none') 
+        plt.margins(0, 0, tight=True)
+
+        github_url = 'https://github.com/google/fonts/blob/master/ofl/titilliumweb/TitilliumWeb-Regular.ttf'
+        url = github_url + '?raw=true'
+        response = urlopen(url)
+        f = NamedTemporaryFile(delete=False, suffix='.ttf')
+        f.write(response.read())
+        f.close()
+        prop = fm.FontProperties(fname=f.name, size=15)
+
+        plt.xlabel(xlabel, fontproperties=prop, size=20)
+        plt.ylabel(ylabel, fontproperties=prop, size=20)
+        plt.title(title, fontproperties=prop, size=30, loc='Center', pad=20)
+
         plt.show()
